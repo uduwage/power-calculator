df1ddb00f8e9ecf88a519a34eb244e141fddd196
Anuradha Uduwage <auduwage@gmail.com>
2026-04-04 19:15:33 -0400

    Initial refactoring adding core module and classes with types.py

---
+++ power_calculator/core/types.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+from typing import Literal
+
+MetricFamily = Literal[
+    "binary",
+    "continous_mean"
+    "count_mean",
+    "ratio_mean",
+]
+
+Alternative = Literal["one-sided", "two-sided"]
+Correction = Literal["none", "bonferroni", "sidak"]
+DesignMode = Literal["sample_size", "power", "mde", "duration"]
+
+EvaluationMode = Literal[
+    "analyze",
+    "confidence_interval",
+    "significance_test",
+]
+
+__all__ = [
+    "MetricFamily",
+    "Alternative",
+    "Correction",
+    "DesignMode",
+    "EvaluationMode",
+]
